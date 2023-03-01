739ff97bd574161c264ef762861c9f484bc3dae5
Krunal-B <123229002+Krunal-B@users.noreply.github.com>
2023-03-02 00:04:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,72 @@
+const userForm = document.getElementById("user-form");
+const userEntries = retrieveEntries();
+const details = document.getElementById("user-entries");
 
-// eslint-disable-next-line no-unused-vars
-const http = require('http')
-// eslint-disable-next-line no-unused-vars
-const fs = require('fs')
-fs.readFile('home.html', (_err, home) => {
-  console.log(home.toString())
-})
-fs.readFile('home.html', (err, home) => {
-  if (err) {
-    throw err
+function retrieveEntries() {
+  return JSON.parse(localStorage.getItem("UserEntries") || "[]");
+}
+
+function displayEntries(entries) {
+  const tableEntries = entries
+    .map(({ name, email, password, dob, acceptTerms }) => `
+      <tr>
+        <td class="border px-4 py-2">${name}</td>
+        <td class="border px-4 py-2">${email}</td>
+        <td class="border px-4 py-2">${password}</td>
+        <td class="border px-4 py-2">${dob}</td>
+        <td class="border px-4 py-2">${acceptTerms}</td>
+      </tr>
+    `)
+    .join("\n");
+
+  const table = `
+    <table class="table-auto w-full">
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Name</th>
+          <th class="px-4 py-2">Email</th>
+          <th class="px-4 py-2">Password</th>
+          <th class="px-4 py-2">Dob</th>
+          <th class="px-4 py-2">Accepted terms?</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tableEntries}
+      </tbody>
+    </table>
+  `;
+
+  details.innerHTML = table;
+}
+
+function saveUserForm(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("dob").value;
+  const acceptTerms = document.getElementById("acceptTerms").checked;
+
+  const currentYear = new Date().getFullYear();
+  const birthYear = dob.split("-");
+  const year = birthYear[0];
+  const age = currentYear - year;
+
+  if (age < 18 || age > 55) {
+    document.getElementById("dob").style.border = "1px solid red";
+    return alert("You must be between 18 and 55 years old to register");
   }
-  http
-    .createServer((request, response) => {
-      response.writeHeader(200, { 'Content-Type': 'text/html' })
-      response.write(home)
-      response.end()
-    })
-    .listen(3000)
-})
 
-let homeContent = ''
-let projectContent = ''
+  document.getElementById("dob").style.border = "none";
 
-fs.readFile('home.html', (err, home) => {
-  if (err) {
-    throw err
-  }
-  homeContent = home
-})
+  const entry = { name, email, password, dob, acceptTerms };
+  userEntries.push(entry);
 
-fs.readFile('project.html', (err, project) => {
-  if (err) {
-    throw err
-  }
-  projectContent = project
-})
-http
-  .createServer((request, response) => {
-    const url = request.url
-    response.writeHeader(200, { 'Content-Type': 'text/html' })
-    switch (url) {
-      case '/project':
-        response.write(projectContent)
-        response.end()
-        break
-      default:
-        response.write(homeContent)
-        response.end()
-        break
-    }
-  })
-  .listen(5000)
+  localStorage.setItem("UserEntries", JSON.stringify(userEntries));
+  displayEntries(userEntries);
+  userForm.reset();
+}
+
+userForm.addEventListener("submit", saveUserForm);
+displayEntries(userEntries);
