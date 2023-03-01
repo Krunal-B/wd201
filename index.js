e38c1b64b437b0b36b36b0b5289d936c34f0a6d6
Krunal-B <123229002+Krunal-B@users.noreply.github.com>
2023-03-01 23:55:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+// eslint-disable-next-line no-unused-vars
+const http = require('http')
+// eslint-disable-next-line no-unused-vars
+const fs = require('fs')
+fs.readFile('home.html', (_err, home) => {
+  console.log(home.toString())
+})
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err
+  }
+  http
+    .createServer((request, response) => {
+      response.writeHeader(200, { 'Content-Type': 'text/html' })
+      response.write(home)
+      response.end()
+    })
+    .listen(3000)
+})
+
+let homeContent = ''
+let projectContent = ''
+
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err
+  }
+  homeContent = home
+})
+
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    throw err
+  }
+  projectContent = project
+})
+http
+  .createServer((request, response) => {
+    const url = request.url
+    response.writeHeader(200, { 'Content-Type': 'text/html' })
+    switch (url) {
+      case '/project':
+        response.write(projectContent)
+        response.end()
+        break
+      default:
+        response.write(homeContent)
+        response.end()
+        break
+    }
+  })
+  .listen(5000)
